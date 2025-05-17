416b9d7efaba3b96f2b1db1939ea62f308e6fbb7
giulia barra <giulia.barra@itiscuneo.eu>
2025-05-17 21:45:27 +0200

    Add project files.

---
+++ WebApplicationPokemon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationPokemon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
