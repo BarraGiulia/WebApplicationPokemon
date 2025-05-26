e93bd54269a7da691bf7279ee305378f5751a245
giulia barra <giulia.barra@itiscuneo.eu>
2025-05-26 18:20:34 +0200

    login/logout funzionante

--- WebApplicationPokemon/Program.cs
+++ WebApplicationPokemon/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using WebApplicationPokemon.Models;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Aggiungi il DbContext e collega al DB
@@ -11,12 +12,19 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddSession();
+builder.Services.AddDistributedMemoryCache(); // necessario per la sessione
+builder.Services.AddHttpContextAccessor(); // prima di builder.Build()
+
+
+
 var app = builder.Build();
 
 // ... configurazioni HTTP già pronte ...
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseSession();
 
 app.UseAuthorization();
 
