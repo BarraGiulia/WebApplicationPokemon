8bd87c2fef5bad1710990e8858c263c30bca4f6a
giulia barra <giulia.barra@itiscuneo.eu>
2025-05-29 20:51:58 +0200

    aggiustato tutta parte delle palestre

--- WebApplicationPokemon/Models/Pokemon.cs
+++ WebApplicationPokemon/Models/Pokemon.cs
@@ -6,15 +6,18 @@ namespace WebApplicationPokemon.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Il nome del Pokémon è obbligatorio")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Il tipo è obbligatorio")]
         public string Tipo { get; set; }
 
+        [Range(1, 100, ErrorMessage = "Il livello deve essere tra 1 e 100")]
         public int Livello { get; set; }
 
-        // Foreign Key verso Trainer
+        // Collegato all'allenatore
         public int TrainerId { get; set; }
         public Trainer Trainer { get; set; }
     }
+
 }
